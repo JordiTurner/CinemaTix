489aa53ba75fad6035efd39e6f4655a85b9fe92f
Jordi Turner <jorditurner@jordis-mbp.dhcp.wustl.edu>
2016-01-26 19:40:00 -0600

    Posters load

--- MovieViewer/MovieViewer/MovieCell.swift
+++ MovieViewer/MovieViewer/MovieCell.swift
@@ -16,6 +16,7 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var overviewLabel: UILabel!
     
     
+    @IBOutlet weak var posterView: UIImageView!
     
     
     override func awakeFromNib() {
